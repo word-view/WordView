3a046ddbb364327f24c79d38945c1019697a4ce0
Arthur Araujo <arthur.araujo@tutanota.com>
2023-10-12 06:17:53 -0300

    chore: remove unused tests

--- tests/api/lessons.test.ts
+++
@@ -1,8 +0,0 @@
-import { getLessons } from "../modules/api";
-
-it(`List lessons by starter difficulty`, async () => {
-  const { status, data } = await getLessons("starter", "dev");
-
-  expect(status).toBe(200);
-  expect(data).toBeDefined();
-});

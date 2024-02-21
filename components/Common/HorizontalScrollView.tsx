4f1131e6a57644c356adfb8ae9369f5c8095afa6
Arthur Araujo <arthur.araujo@tutanota.com>
2024-02-21 16:36:53 -0300

    feat: allow contentContainer styling on HorizontalScrollView

--- components/Common/HorizontalScrollView.tsx
+++ components/Common/HorizontalScrollView.tsx
@@ -3,12 +3,14 @@ import { ScrollView, StyleProp, ViewStyle } from 'react-native'
 interface HorizontalScrollViewProps {
   children: any
   style?: StyleProp<ViewStyle>
+  contentContainerStyle?: StyleProp<ViewStyle>
 }
 
 export function HorizontalScrollView(props: HorizontalScrollViewProps) {
   return (
     <ScrollView
       style={props.style}
+      contentContainerStyle={props.contentContainerStyle}
       horizontal={true}
       showsHorizontalScrollIndicator={false}
     >

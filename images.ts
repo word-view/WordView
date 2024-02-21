97fe9b392d645b9a0fe810076394f6b24fc8b8b1
Arthur Araujo <arthur.araujo@tutanota.com>
2024-02-21 16:34:37 -0300

    chore: add placeholder song covers

--- images.ts
+++ images.ts
@@ -2,6 +2,9 @@ const images = {
   wvIcon: require('./assets/wvicon.png'),
   wvTitle: require('./assets/wvtitle.png'),
   cac: require('./assets/builtin/cac.png'),
+  mirrorCover: require('./assets/builtin/Mirror_Cover.jpg'),
+  yoruNiCover: require('./assets/builtin/YoruNi_Cover.jpeg'),
+  kataomoiCover: require('./assets/builtin/Kataomoi_Cover.jpeg')
 }
 
 export default images

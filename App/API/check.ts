c8aa7a737201d9c33420676b6007d9e9cd09e185
Arthur Araujo <arthur.araujo@tutanota.com>
2024-04-11 00:37:17 -0300

    Add new API module

---
+++ App/API/check.ts
@@ -0,0 +1,18 @@
+import { get } from './client'
+
+/**
+ * Checks the availability of WordView's API.
+ * @returns {boolean} Returns true if the API is reachable; otherwise, false.
+ */
+export async function checkAPIAvailable(): Promise<boolean> {
+  try {
+    const response = await get('/ping')
+
+    if (response.status === 200 && (await response.text()) === 'Not dead yet!') {
+      return true
+    } else return false
+  } catch (error) {
+    console.error('The API could not be reached \n\n', error)
+    return false
+  }
+}
---
+++ App/API/client.ts
@@ -0,0 +1,24 @@
+const API_URL_DEV = 'http://localhost:8080/api/v1'
+const API_URL_PROD = 'https://wordview.cc/api/v1'
+
+export async function post(url: string, body: string) {
+  return await fetch(API_URL_DEV + url, {
+    method: 'POST',
+    credentials: 'omit',
+    mode: 'cors',
+    headers: {
+      Accept: '*/*',
+      'Accept-Language': 'en-US,en;q=0.5',
+      'Content-Type': 'application/json',
+    },
+    body: body,
+  })
+}
+
+export async function get(url: string) {
+  return await fetch(API_URL_DEV + url)
+}
+
+export function url(path: string) {
+  return API_URL_DEV + path
+}

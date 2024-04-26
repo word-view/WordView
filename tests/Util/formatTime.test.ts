35c861fd80a7a689488ace0f00e1de778fe1f156
Arthur Araujo <arthur.araujo@tutanota.com>
2024-04-26 07:05:28 -0300

    Add format test for NaN

--- tests/Util/formatTime.test.ts
+++ tests/Util/formatTime.test.ts
@@ -1,16 +1,21 @@
 import { formatTime } from '../../App/Util/time'
 
-it(`Seconds`, () => {
+it(`Format time 1 second`, () => {
   let time = formatTime(1000)
   expect(time).toBe('00:01')
 })
 
-it(`Minutes`, () => {
+it(`Format time 1 minute`, () => {
   let time = formatTime(60000)
   expect(time).toBe('01:00')
 })
 
-it(`Hours`, () => {
+it(`Format time 1 hour`, () => {
   let time = formatTime(3600000)
   expect(time).toBe('01:00:00')
+})
+
+it(`Format time NaN`, () => {
+  let time = formatTime(NaN)
+  expect(time).toBe('00:00')
 })

314733733212ca2cbd7c1393b15454d9aeaa3e2b
Arthur Araujo <arthur.araujo@tutanota.com>
2024-04-26 07:48:01 -0300

    Skip App renders test in CI

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -4,6 +4,8 @@ import App from '../App'
 // This takes a long time to run but it still useful. Should probably find a way
 // to test individual screens instead of the entire app.
 it(`App renders`, () => {
+  if (process.env.CI) return
+
   const tree = renderer.create(<App />).toJSON()
   expect(tree).toBeDefined()
 })

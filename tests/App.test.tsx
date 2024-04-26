1b4f7c5ee9f43cedb0a5db1e9ff7bb8bc5e6f4d3
Arthur Araujo <arthur.araujo@tutanota.com>
2024-04-26 07:45:12 -0300

    Add entire App component test

---
+++ tests/App.test.tsx
@@ -0,0 +1,9 @@
+import renderer from 'react-test-renderer'
+import App from '../App'
+
+// This takes a long time to run but it still useful. Should probably find a way
+// to test individual screens instead of the entire app.
+it(`App renders`, () => {
+  const tree = renderer.create(<App />).toJSON()
+  expect(tree).toBeDefined()
+})

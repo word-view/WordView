31fafa19a26ebdf526e7da501814eddc0b5da028
Arthur Araujo <arthur.araujo@tutanota.com>
2024-05-19 12:05:46 -0300

    Redesign the player

--- App/Components/Player/PlayButton.tsx
+++ App/Components/Player/PlayButton.tsx
@@ -1,17 +1,36 @@
 import { memo } from 'react';
-import { Appbar } from 'react-native-paper';
+import { Appbar, Tooltip } from 'react-native-paper';
 
 interface PlayButtonProps {
     isAudioPlaying: boolean;
+    size: number;
     onPause: () => void;
     onPlay: () => void;
 }
 
 function $PlayButton(props: PlayButtonProps) {
     if (props.isAudioPlaying) {
-        return <Appbar.Action icon='pause' size={32} onPress={props.onPause} />;
+        return (
+            <Tooltip title='Pausar (K)'>
+                <Appbar.Action
+                    style={{ padding: 0, margin: 0 }}
+                    icon='pause'
+                    size={props.size}
+                    onPress={props.onPause}
+                />
+            </Tooltip>
+        );
     } else {
-        return <Appbar.Action icon='play' size={32} onPress={props.onPlay} />;
+        return (
+            <Tooltip title='Reproduzir (K)'>
+                <Appbar.Action
+                    style={{ padding: 0, margin: 0 }}
+                    icon='play'
+                    size={props.size}
+                    onPress={props.onPlay}
+                />
+            </Tooltip>
+        );
     }
 }
 

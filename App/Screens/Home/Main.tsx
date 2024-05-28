6ab953572c65c2b8d4e0569c65a659f7181fe373
Arthur Araujo <arthur.araujo@tutanota.com>
2024-05-28 16:44:43 -0300

    Add no internet fallback screen

--- App/Screens/Home/Main.tsx
+++ App/Screens/Home/Main.tsx
@@ -1,9 +1,12 @@
 import { memo, useState } from 'react';
-import { Image, ScrollView } from 'react-native';
+import { Image, ScrollView, StyleSheet } from 'react-native';
+import { Text } from 'react-native-paper';
 import { ContinueHearing, Welcome } from './Sections';
 import { onMountAsync } from '../../../Framework/Components/Actions';
 import { checkAPIAvailable } from '../../API/check';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import images from '../../image';
+import { colors } from '../../colors';
 
 interface MainScreenProps {
     marginLeft?: number;
@@ -13,15 +16,28 @@ interface MainScreenProps {
 
 function Main(props: MainScreenProps) {
     const isFirstLaunch = false;
-    const [connectionAvailable, setConnectionAvailable] = useState<boolean>();
+    const [connectionAvailable, setConnectionAvailable] = useState<boolean>(true);
 
-    onMountAsync(async () => {
-        setConnectionAvailable(await checkAPIAvailable());
-        console.log(await checkAPIAvailable());
-    });
+    onMountAsync(async () => setConnectionAvailable(await checkAPIAvailable()));
 
     if (!connectionAvailable) {
-        return <ScrollView></ScrollView>;
+        return (
+            <ScrollView
+                style={{ marginLeft: wp(props.marginLeft ?? 0) }}
+                contentContainerStyle={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Image style={styles.radio} source={images.radio} />
+                <Text variant='headlineMedium' style={styles.errorTitle}>
+                    Sem acesso a internet
+                </Text>
+                <Text variant='headlineSmall' style={styles.errorDescription}>
+                    Não foi possível acessar o servidor. {'\n'} Verifique sua conexão com a internet
+                </Text>
+            </ScrollView>
+        );
     }
 
     return (
@@ -44,3 +60,23 @@ function Main(props: MainScreenProps) {
 }
 
 export default memo(Main);
+
+const styles = StyleSheet.create({
+    radio: {
+        height: 320,
+        width: 300,
+        marginTop: 20,
+    },
+    errorTitle: {
+        fontFamily: 'OpenSans',
+        fontWeight: '700',
+        marginTop: 12,
+        color: colors.icon,
+    },
+    errorDescription: {
+        fontFamily: 'OpenSans',
+        marginTop: 4,
+        textAlign: 'center',
+        color: colors.iconDarker,
+    },
+});
--- App/image.ts
+++ App/image.ts
@@ -2,6 +2,7 @@ const images = {
     wvIcon: require('../assets/wvicon.png'),
     wvTitle: require('../assets/wvtitle.png'),
     cac: require('../assets/builtin/cac.png'),
+    radio: require('../assets/radio.png'),
     mirrorCover: require('../assets/builtin/Mirror_Cover.jpg'),
     yoruNiCover: require('../assets/builtin/YoruNi_Cover.jpeg'),
     kataomoiCover: require('../assets/builtin/Kataomoi_Cover.jpeg'),
